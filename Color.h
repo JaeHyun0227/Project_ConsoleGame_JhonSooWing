6a0717ab990532109ed4c5e19d0c001e75a50ac5
JaeHyun0227 <shr7003@gmail.com>
2019-07-01 11:54:37 +0900

    Color 헤더파일
    색 정의

---
+++ Color.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Windows.h>
+
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define OK 3
+#define RED 4
+#define PURPLE 5
+#define YELLOW 6
+#define WHITE 7
+#define GRAY 8
+#define LIGHTBLUE 9
+#define LIGHTGREEN 10
+#define LIGHTOK 11
+#define LIGHTRED 12
+#define LIGHTPURPLE 13
+#define LIGHTYELLO 14
+#define LIGHTWHITE 15
+
+class Color
+{
+public:
+
+
+
+};	
+
